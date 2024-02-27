c55c45879836f4154f67df358c2cb489e9b73347
rregineanne <rcreyes18@up.edu.ph>
2024-02-27 17:37:32 +0800

    Draft of exercise 1

--- Exercise 1/Exercise 1_Reyes.py
+++ Exercise 1/Exercise 1_Reyes.py
@@ -1,37 +1,46 @@
 """
-GE 120 Introduction Lecture 2
+GE 120 Lecture 2
+Machine Exercise 1
 Regine Ann Reyes
 February 27, 2024
 """
+# PART 1: Convert dd to dms
 
-# convert dd to dms
 dd = 118.42069
 # extract the degree part
 degree = int(dd)
-# print("degree:", degree)
+print("degree:", degree)
 
 # extract the minutes
-# minutes = dms - degree
+minutes = dd - degree
 minutes = (dd-degree) * 60
 
 minutes_whole = int(minutes)
 
 seconds = (minutes - minutes_whole) * 60
 
-# print("minutes but in degrees:",minutes)
-# print("minutes", minutes_whole)
-# print("seconds:", seconds)
-# print("dms:" + str(degree) + "-" + str(minutes_whole) + "-" + str(round(seconds,2)))
+print("minutes but in degrees:",minutes)
+print("minutes but whole:", minutes_whole)
+print("seconds:", seconds)
 
-# convert dms to dd
+# get dms value by combining the degrees, minutes, and seconds through string
+print("dms value:", str(degree) + "-" + str(minutes_whole) + "-" + str(round(seconds,2)))
+
+
+
+# PART 2: Convert dms to dd
+
 # dms = 118-25-14.48
 dms = "118-25-14.48"
 values = dms.split("-")
+print("list of values in dms form:", values)
 
+# identify values from the list by splitting
 degrees = int(values[0])
 minutes = int(values[1])
 seconds = float(values[2])
 
+# get dd value by converting the minutes and seconds to decimal form
 dd = degrees + (minutes/60) + (seconds/3600)
 
 print("dd value:", round(dd,6))
