a4b10e65df0579141ce5a989405fcb46fb15a65d
rregineanne <rcreyes18@up.edu.ph>
2024-02-27 17:05:09 +0800

    Draft of exercise 1

--- Exercise 1/Exercise 1_Reyes.py
+++ Exercise 1/Exercise 1_Reyes.py
@@ -1,5 +1,35 @@
---
+"""
 GE 120 Introduction Lecture 2
 Regine Ann Reyes
 February 27, 2024
---
+"""
+
+dms = 118.42069
+# extract the degree part
+degree = int(dms)
+# print("degree:", degree)
+
+# extract the minutes
+# minutes = dms - degree
+minutes = (dms-degree) * 60
+
+minutes_whole = int(minutes)
+
+seconds = (minutes - minutes_whole) * 60
+
+# print("minutes but in degrees:",minutes)
+# print("minutes", minutes_whole)
+# print("seconds:", seconds)
+# print("dms:" + str(degree) + "-" + str(minutes_whole) + "-" + str(round(seconds,2)))
+
+# dms = 118-25-14.48
+dms = "118-25-14.48"
+values = dms.split("-")
+
+degrees = int(values[0])
+minutes = int(values[1])
+seconds = float(values[2])
+
+dd = degrees + (minutes/60) + (seconds/3600)
+
+print("dd value:", round(dd,6))
