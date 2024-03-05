87e7ef17500aa821e7a1d7432868ed0d16d54e70
rregineanne <rcreyes18@up.edu.ph>
2024-03-05 15:44:06 +0800

    Final draft of exercise 1

--- Exercise 1/Exercise 1_Reyes.py
+++ Exercise 1/Exercise 1_Reyes.py
@@ -1,38 +1,37 @@
 """
-GE 120 Lecture 2
-Machine Exercise 1
+GE 120: Machine Exercise 1
 Regine Ann Reyes
 February 27, 2024
 """
-# PART 1: Convert dd to dms
+# PART 1: Convert DD to DMS
 
-dd = 118.42069
+# input a decimal degree value
+DD = float(input("Enter Decimal Degree (Ex. 118.42069): "))
+
 # extract the degree part
-degree = int(dd)
+degree = int(DD)
 print("degree:", degree)
 
 # extract the minutes
-minutes = dd - degree
-minutes = (dd-degree) * 60
-
+minutes = DD - degree
+minutes = (DD-degree) * 60
 minutes_whole = int(minutes)
-
-seconds = (minutes - minutes_whole) * 60
-
-print("minutes but in degrees:",minutes)
+print("minutes but in degrees:", minutes)
 print("minutes but whole:", minutes_whole)
+
+# extract the seconds
+seconds = (minutes - minutes_whole) * 60
 print("seconds:", seconds)
 
 # get dms value by combining the degrees, minutes, and seconds through string
-print("dms value:", str(degree) + "-" + str(minutes_whole) + "-" + str(round(seconds,2)))
+print("Degree, Minute, and Second Value:", str(degree) + "-" + str(minutes_whole) + "-" + str(round(seconds,2)))
 
 
+# PART 2: Convert DMS to DD
 
-# PART 2: Convert dms to dd
-
-# dms = 118-25-14.48
-dms = "118-25-14.48"
-values = dms.split("-")
+# input a degree, minute, and second value
+DMS = input("Enter Degree, Minute, and Second Value (Ex. 118-25-14.48): ")
+values = DMS.split("-")
 print("list of values in dms form:", values)
 
 # identify values from the list by splitting
@@ -42,5 +41,4 @@ seconds = float(values[2])
 
 # get dd value by converting the minutes and seconds to decimal form
 dd = degrees + (minutes/60) + (seconds/3600)
-
-print("dd value:", round(dd,6))
+print("Decimal Degree Value:", round(dd,6))
