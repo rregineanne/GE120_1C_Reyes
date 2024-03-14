fa62af860e6827fbf72bf5a525cd855fb0d9b337
rregineanne <rcreyes18@up.edu.ph>
2024-03-14 17:06:34 +0800

    Added lecture 4 notes

---
+++ Lecture4_Reyes.py
@@ -0,0 +1,12 @@
+# def shout() :
+   # word = "Hi"
+  #  print(word+"!")
+
+# word = "IAN"
+# shout()
+
+def shout(word1, word2) :
+    print(word1+"!")
+    print(word2+"...")
+
+shout("Mafe", "Omar")
