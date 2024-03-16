e47b452db3604dd7557f7bed82ac87c987cf0c8d
rregineanne <rcreyes18@up.edu.ph>
2024-03-16 19:36:35 +0800

    Draft of exercise 3

--- Exercise 3/Exercise 3_Reyes.py
+++ Exercise 3/Exercise 3_Reyes.py
@@ -5,7 +5,7 @@ March 14, 2024
 """
 
 from math import cos, sin, radians, sqrt
-# get from Google
+# Get from Google
 
 def getLatitude(distance,azimuth) :
     '''
@@ -41,7 +41,7 @@ def getDeparture(distance, azimuth) :
 
 def AzimuthToBearing(azimuth) :
     '''
-        Compute for departure of a given line.
+        Compute for the DMS bearing of a given angle.
 
         Input :
         azimuth - float
@@ -49,183 +49,194 @@ def AzimuthToBearing(azimuth) :
         Output:
         bearing - string
         '''
+    if "-" in azimuth : # In DMS form
+                # Convert DMS to DD
+                dms = azimuth
+                degrees, minutes, seconds = azimuth.split("-")
+                degrees, minutes, seconds = float(degrees), float(minutes), float(seconds)
+                azimuth = (degrees+(minutes/60)+(seconds/3600)) % 360 
 
+                # Identify the bearing and orientation of the DMS angle
+                # 1st - convert azimuth to bearing in DD, and  identify direction
+                # 2nd - convert the DD angle from 1st to DMS
+                if azimuth > 0 and azimuth < 90:
+                    degree = int(azimuth)
+                    minutes = (azimuth - degree) * 60
+                    minutes_whole = int(minutes)
+                    seconds = round(float((minutes - minutes_whole) * 60),2)
+                    dms = f"{degree}-{minutes_whole}-{seconds}"
+                    bearing = 'S {: ^5} W'.format(dms)
+                elif azimuth > 90 and azimuth < 180:
+                    azimuth = 180 - azimuth
+                    azimuth = float(azimuth)
+                    degree = int(azimuth)
+                    minutes = (azimuth - degree) * 60
+                    minutes_whole = int(minutes)
+                    seconds = round(float((minutes - minutes_whole) * 60),2)
+                    dms = f"{degree}-{minutes_whole}-{seconds}"
+                    bearing = 'N {: ^5} W'.format(dms)
+                elif azimuth > 180 and azimuth < 270:
+                    azimuth = azimuth - 180
+                    azimuth = float(azimuth)
+                    degree = int(azimuth)
+                    minutes = (azimuth - degree) * 60
+                    minutes_whole = int(minutes)
+                    seconds = round(float((minutes - minutes_whole) * 60),2)
+                    dms = f"{degree}-{minutes_whole}-{seconds}"
+                    bearing = 'N {: ^5} E'.format(dms)
+                elif azimuth > 270 and azimuth < 360:
+                    azimuth = 360 - azimuth
+                    azimuth = float(azimuth)
+                    degree = int(azimuth)
+                    minutes = (azimuth - degree) * 60
+                    minutes_whole = int(minutes)
+                    seconds = round(float((minutes - minutes_whole) * 60),2)
+                    dms = f"{degree}-{minutes_whole}-{seconds}"
+                    bearing = 'S {: ^5} E'.format(dms)
+                elif azimuth == 0:
+                    bearing = "DUE SOUTH"
+                elif azimuth == 90:
+                    bearing = "DUE WEST"
+                elif azimuth == 180:
+                    bearing = "DUE NORTH"
+                elif azimuth == 270:
+                    bearing = "DUE EAST"
+                elif azimuth == 360:
+                    bearing = "DUE SOUTH"
+
+    else : # In DD form 
+                # Convert DD to DMS
+                azimuth = float(azimuth) 
+                azimuth = (azimuth) % 360
+
+                # Identify the bearing and orientation of the DD angle
+                # 1st - convert azimuth to bearing in DD, and  identify direction
+                # 2nd - convert the DD angle from 1st to DMS
+                if azimuth > 0 and azimuth < 90:
+                    degree = int(azimuth)
+                    minutes = (azimuth - degree) * 60
+                    minutes_whole = int(minutes)
+                    seconds = round(float((minutes - minutes_whole) * 60),2)
+                    dms = f"{degree}-{minutes_whole}-{seconds}"
+                    bearing = 'S {: ^5} W'.format(dms)
+                elif azimuth > 90 and azimuth < 180:
+                    azimuth = 180 - azimuth
+                    azimuth = float(azimuth)
+                    degree = int(azimuth)
+                    minutes = (azimuth - degree) * 60
+                    minutes_whole = int(minutes)
+                    seconds = round(float((minutes - minutes_whole) * 60),2)
+                    dms = f"{degree}-{minutes_whole}-{seconds}"
+                    bearing = 'N {: ^5} W'.format(dms)
+                elif azimuth > 180 and azimuth < 270:
+                    azimuth = azimuth - 180
+                    azimuth = float(azimuth)
+                    degree = int(azimuth)
+                    minutes = (azimuth - degree) * 60
+                    minutes_whole = int(minutes)
+                    seconds = round(float((minutes - minutes_whole) * 60),2)
+                    dms = f"{degree}-{minutes_whole}-{seconds}"
+                    bearing = 'N {: ^5} E'.format(dms)
+                elif azimuth > 270 and azimuth < 360:
+                    azimuth = 360 - azimuth
+                    azimuth = float(azimuth)
+                    degree = int(azimuth)
+                    minutes = (azimuth - degree) * 60
+                    minutes_whole = int(minutes)
+                    seconds = round(float((minutes - minutes_whole) * 60),2)
+                    dms = f"{degree}-{minutes_whole}-{seconds}"
+                    bearing = 'S {: ^5} E'.format(dms)
+                elif azimuth == 0:
+                    bearing = "DUE SOUTH"
+                elif azimuth == 90:
+                    bearing = "DUE WEST"
+                elif azimuth == 180:
+                    bearing = "DUE NORTH"
+                elif azimuth == 270:
+                    bearing = "DUE EAST"
+                elif azimuth == 360:
+                    bearing = "DUE SOUTH"
+    
+    return bearing
+
+# Create a sentinel controlled loop
 counter1 = 1
 counter2 = 2
 counter = f"{counter1}-{counter2}"
 lines = []
+sumLat = 0
+sumDep = 0
+sumDist = 0  
 
 while True :
-    print()
-    print("LINE NO. ", counter)    
-    # ask for distance input
-    try:
-        distance_input = float(input("Distance: "))
-    # if distance is invalid, ask for input again
-    except ValueError :
-        print("INVALID: You need to enter a number.")
         print()
-    else :
-        azimuth = input("Azimuth from the South: ")
+        print("LINE NO. ", counter)    
+        # Ask for distance input
+        try:
+            distance_input = float(input("Enter Distance: "))
+        # If distance is invalid, ask for input again
+        except ValueError :
+            print("INVALID: You need to enter a number.")
+            print()
+        else :
+            azimuth = input("Enter Azimuth from the South: ")
 
-        if "-" in azimuth : # in DMS form
-            # convert DMS to DD
-            dms = azimuth
-            degrees, minutes, seconds = azimuth.split("-")
-            degrees, minutes, seconds = float(degrees), float(minutes), float(seconds)
-            azimuth = (degrees+(minutes/60)+(seconds/3600)) % 360 
+        # Extract bearing, lat, and dep values
+        bearing = AzimuthToBearing(azimuth)
+        latitude = getLatitude(azimuth=float(azimuth), distance=float(distance_input)) 
+        departure = getDeparture(azimuth=float(azimuth), distance=float(distance_input))
 
-            # identify the bearing and orientation of the decimal degree angle
-            if azimuth > 0 and azimuth < 90:
-                degree = int(azimuth)
-                minutes = (azimuth - degree) * 60
-                minutes_whole = int(minutes)
-                seconds = round(float((minutes - minutes_whole) * 60),2)
-                dms = f"{degree}-{minutes_whole}-{seconds}"
-                bearing = 'S {: ^5} W'.format(dms)
-            elif azimuth > 90 and azimuth < 180:
-                azimuth = 180 - azimuth
-                azimuth = float(azimuth)
-                degree = int(azimuth)
-                minutes = (azimuth - degree) * 60
-                minutes_whole = int(minutes)
-                seconds = round(float((minutes - minutes_whole) * 60),2)
-                dms = f"{degree}-{minutes_whole}-{seconds}"
-                bearing = 'N {: ^5} W'.format(dms)
-            elif azimuth > 180 and azimuth < 270:
-                azimuth = azimuth - 180
-                azimuth = float(azimuth)
-                degree = int(azimuth)
-                minutes = (azimuth - degree) * 60
-                minutes_whole = int(minutes)
-                seconds = round(float((minutes - minutes_whole) * 60),2)
-                dms = f"{degree}-{minutes_whole}-{seconds}"
-                bearing = 'N {: ^5} E'.format(dms)
-            elif azimuth > 270 and azimuth < 360:
-                azimuth = 360 - azimuth
-                azimuth = float(azimuth)
-                degree = int(azimuth)
-                minutes = (azimuth - degree) * 60
-                minutes_whole = int(minutes)
-                seconds = round(float((minutes - minutes_whole) * 60),2)
-                dms = f"{degree}-{minutes_whole}-{seconds}"
-                bearing = 'S {: ^5} E'.format(dms)
-            elif azimuth == 0:
-                bearing = "DUE SOUTH"
-            elif azimuth == 90:
-                bearing = "DUE WEST"
-            elif azimuth == 180:
-                bearing = "DUE NORTH"
-            elif azimuth == 270:
-                bearing = "DUE EAST"
-            elif azimuth == 360:
-                bearing = "DUE SOUTH"
+        # Get summation of latitude and departure
+        sumLat += latitude
+            # sumLat = sumLat + Lat
+        sumDep += departure
+            # sumDep + sumDep + Dep
+        sumDist = float(distance_input)
 
-        else : # in DD form 
-            # convert DD to DMS
-            azimuth = float(azimuth) 
-            azimuth = (azimuth) % 360
+        line = (counter, distance_input, bearing, latitude, departure)  # Create tuple of the line
+        lines.append(line)
 
-            # identify the bearing and orientation of the decimal degree angle
-            if azimuth > 0 and azimuth < 90:
-                degree = int(azimuth)
-                minutes = (azimuth - degree) * 60
-                minutes_whole = int(minutes)
-                seconds = round(float((minutes - minutes_whole) * 60),2)
-                dms = f"{degree}-{minutes_whole}-{seconds}"
-                bearing = 'S {: ^5} W'.format(dms)
-            elif azimuth > 90 and azimuth < 180:
-                azimuth = 180 - azimuth
-                azimuth = float(azimuth)
-                degree = int(azimuth)
-                minutes = (azimuth - degree) * 60
-                minutes_whole = int(minutes)
-                seconds = round(float((minutes - minutes_whole) * 60),2)
-                dms = f"{degree}-{minutes_whole}-{seconds}"
-                bearing = 'N {: ^5} W'.format(dms)
-            elif azimuth > 180 and azimuth < 270:
-                azimuth = azimuth - 180
-                azimuth = float(azimuth)
-                degree = int(azimuth)
-                minutes = (azimuth - degree) * 60
-                minutes_whole = int(minutes)
-                seconds = round(float((minutes - minutes_whole) * 60),2)
-                dms = f"{degree}-{minutes_whole}-{seconds}"
-                bearing = 'N {: ^5} E'.format(dms)
-            elif azimuth > 270 and azimuth < 360:
-                azimuth = 360 - azimuth
-                azimuth = float(azimuth)
-                degree = int(azimuth)
-                minutes = (azimuth - degree) * 60
-                minutes_whole = int(minutes)
-                seconds = round(float((minutes - minutes_whole) * 60),2)
-                dms = f"{degree}-{minutes_whole}-{seconds}"
-                bearing = 'S {: ^5} E'.format(dms)
-            elif azimuth == 0:
-                bearing = "DUE SOUTH"
-            elif azimuth == 90:
-                bearing = "DUE WEST"
-            elif azimuth == 180:
-                bearing = "DUE NORTH"
-            elif azimuth == 270:
-                bearing = "DUE EAST"
-            elif azimuth == 360:
-                bearing = "DUE SOUTH"
-   
-
-
-    bearing = AzimuthToBearing(azimuth)
-    lat = getLatitude(azimuth=float(azimuth), distance=float(distance_input)) # kulang pa
-    dep = getDeparture(azimuth=float(azimuth), distance=float(distance_input))
-
-# hindi pa tapos
-    sumLat += lat
-    sumDep += dep
-
-
-    line = (counter, distance_input, bearing)  # create tuple of the line
-    lines.append(line)
+    #    lat = getLatitude(12, 160)
+    #    dep = getDeparture(12, 160)
+    #    print(lat)
+    #    print(dep)
 
         # Ask for input
-    YN = input("Add new line? (Y/N) ")
-    if YN.lower() == "yes" or YN.lower() == "y":
-            counter1 += 1
-            counter2 += 1
-            counter = f"{counter1}-{counter2}"
-            continue
-    else:
-            break
-
-lat = getLatitude(12, 160)
-dep = getDeparture(12, 160)
-print(lat)
-print(dep)
-
+        YN = input("Add new line? (Y/N) ")
+        if YN.lower() == "yes" or YN.lower() == "y":
+                counter1 += 1
+                counter2 += 1
+                counter = f"{counter1}-{counter2}"
+                continue
+        else:
+                break
 
 print()
-print("-----------------------------------------------")
-print('{: ^15} {: ^15} {: ^15}'.format("LINE NO. ", "DISTANCE ", "BEARING "))
-print("-----------------------------------------------")
+print("-------------------------------------------------------------------------------------")
+print('{: ^15} {: ^15} {: ^15} {: ^15} {: ^15}'.format("LINE NO. ", "DISTANCE ", "BEARING ", "LATITUDE ", "DEPARTURE "))
+print("-------------------------------------------------------------------------------------")
 
 for line in lines:
-    print('{: ^13} {: ^16} {: ^16}'.format(line[0], line[1], line[2]))
+    print('{: ^13} {: ^16} {: ^16} {: ^18} {: ^18}'.format(line[0], line[1], line[2], line[3], line[4]))
 
-print("---------------------END-----------------------")
+print("-------------------------------------------------------------------------------------")
+print("Summation of Latitude: ", round(sumLat,6))
+print("Summation of Departure: ", round(sumDep,6))
+print("Summation of Distance: ", round(sumDist,3))
 
-lec = sqrt(sumLat**2 + sumDep**2)
+LEC = sqrt(sumLat**2 + sumDep**2)
+print("LEC: ", LEC)
+REC = sumDist/LEC
+print("1: ", (REC)) # round off pa
 
-print("LEC: ", lec)
-rec = sumDist/lec
-print("1: ", round(rec, -3))
+#constCorrLat = sumLat/sumDist
+#constCorrDep = sumDep/sumDist
 
-constCorrLat = sumLat/sumDist
-constCorrDep = sumDep/sumDist
+#for line in lines:
+ #   corr_lat = constCorrLat * line[1]
+  #  corr_dep = constCorrDep * line[1]
 
-for line in lines:
-    corr_lat = constCorrLat * line[1]
-    corr_dep = constCorrDep * line[1]
-
-    adjlat = line[3] + corr_lat
-    adjdep = line[4] + corr_dep
+    # adjlat = line[3] + corr_lat
+    # adjdep = line[4] + corr_dep
 
 print("---------------------END-----------------------")
