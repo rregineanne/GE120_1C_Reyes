fc22e3682945fd968c71fd3e2f2095760c764cc3
rregineanne <rcreyes18@up.edu.ph>
2024-04-11 16:31:41 +0800

    Added lecture notes

--- Lecture5_Reyes.py
+++ Lecture5_Reyes.py
@@ -20,9 +20,27 @@ class MLHero :
             "skill_effects": 50, 
             "difficulty": 70
         } 
-    def skill(self) :
-        print("ATTACK")
+    def __str__(self):
+        return(self.name + ", the " + self.description)
+    
+    def __add__(self, other):
+        return(self.name + " and " + other.name + " combined!")
+    def __gt__(self, other):
+        if (self.statistics["offense" > other.statistics["offense"]]) :
+            else 
 
+    def skill(self):
+        # print("ATTACK")
+        print(self.name + " used the ATTACK!!")
+
+    def superSkill(self, opponent) :
+        print(self.name + "used SUPERSKILL against " + opponent)
+
+    def split(self) :
+        print("Masakit Mag Split")
+        
+lunox = MLHero("Lunox")
+aldous = MLHero("Aldous", "Demon Slayer")
 
 hero = MLHero("LUNOX", "MasipagNaUPStudent")
 print("Hero Name:", hero.name)
@@ -31,3 +49,4 @@ print("Hero Role:", hero.role)
 print("Hero Specialty:", hero.specialty.split("/"))
 print("Hero Offense+difficulty:", hero.statistics["offense"] + hero.statistics["difficulty"])
 
+# hero.superSkill("Zilong")
