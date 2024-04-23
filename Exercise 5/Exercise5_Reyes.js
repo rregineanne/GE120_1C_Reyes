21303db960aa720ce21e95dcd30853556fcf429d
rregineanne <rcreyes18@up.edu.ph>
2024-04-23 20:43:23 +0800

    Draft of exercise 5

--- Exercise 5/Exercise5_Reyes.js
+++ Exercise 5/Exercise5_Reyes.js
@@ -16,11 +16,10 @@ function getLatitude(distance,azimuth) {
     Output{
     latutude - float
     */
-
-    var latitude = - (distance * Math.cos(azimuth * Math.PI / 180.0))
-
-    return latitude
+    if (azimuth % 180 == 90) {return 0} else {    
+    return (-distance * Math.cos(azimuth * Math.PI / 180.0))}
 }
+
 function getDeparture(distance, azimuth) {
     /*
     Compute for departure of a given line.
@@ -32,11 +31,10 @@ function getDeparture(distance, azimuth) {
     Output{
     departure - float
     */
-
-    var departure = - (distance * Math.sin(azimuth * Math.PI / 180.0))
-
-    return departure
+    if (azimuth & 180 == 0) {return 0} else {
+    return (-distance * Math.sin(azimuth * Math.PI / 180.0))}
 }
+
 function AzimuthToBearing(azimuth) {
     /*
         Compute for the DMS bearing of a given angle.
@@ -46,56 +44,53 @@ function AzimuthToBearing(azimuth) {
 
         Output{
         bearing - string
-        */
-    azimuth = 
-          
-    if ( "-" in azimuth ) { // In DMS form
+        */          
+    if (azimuth.includes("-")) { // In DMS form
                 // Convert DMS to DD
-                dms = azimuth
-                degrees, minutes, seconds = azimuth.split("-")
+                let dms = azimuth
+                let [degrees, minutes, seconds] = azimuth.split("-")
                 degrees, minutes, seconds = float(degrees), float(minutes), float(seconds)
                 azimuth = (degrees+(minutes/60)+(seconds/3600)) % 360 
-                azimuth_uncon = azimuth // make a variable of the unconverted azimuth for lat && dep
+                let azimuth_uncon = azimuth // make a variable of the unconverted azimuth for lat and dep
                 /*
                 Identif (y the bearing && orientation of the DMS angle
                 // 1st - convert azimuth to bearing in DD, &&  identif (y direction
                 // 2nd - convert the DD angle from 1st to DMS
                 */
                 if ( azimuth > 0 && azimuth < 90 ) {
-                    degree = int(azimuth)
-                    minutes = (azimuth - degree) * 60
-                    minutes_whole = int(minutes)
-                    seconds = round(float((minutes - minutes_whole) * 60),2)
-                    dms = f"{degree}-{minutes_whole}-{seconds}"
-                    bearing = 'S {{ ^5} W'.format(dms)
-                }    
+                    let degree = int(azimuth)
+                    let minutes = (azimuth - degree) * 60
+                    let minutes_whole = int(minutes)
+                    let seconds = round(float((minutes - minutes_whole) * 60),2)
+                    let dms = '${degree}-${minutes_whole}-${seconds}' // Following the JavaScript for string interpolation
+                    let bearing = 'S ${dms} W'
                 } else if ( azimuth > 90 && azimuth < 180 ) {
                     azimuth = 180 - azimuth
                     azimuth = float(azimuth)
                     degree = int(azimuth)
                     minutes = (azimuth - degree) * 60
                     minutes_whole = int(minutes)
-                    seconds = round(float((minutes - minutes_whole) * 60),2)
-                    dms = f"{degree}-{minutes_whole}-{seconds}"
-                    bearing = 'N {{ ^5} W'.format(dms)
+                    seconds = (float((minutes - minutes_whole) * 60).toPrecision(2))
+                    dms = '${degree}-${minutes_whole}-${seconds}' // Following the JavaScript for string interpolation
+                    bearing = 'N ${dms} W'
                 } else if ( azimuth > 180 && azimuth < 270 ) {
                     azimuth = azimuth - 180
                     azimuth = float(azimuth)
                     degree = int(azimuth)
                     minutes = (azimuth - degree) * 60
                     minutes_whole = int(minutes)
-                    seconds = round(float((minutes - minutes_whole) * 60),2)
-                    dms = f"{degree}-{minutes_whole}-{seconds}"
-                    bearing = 'N {{ ^5} E'.format(dms)
+                    seconds = (float((minutes - minutes_whole) * 60).toPrecision(2))
+                    dms = '${degree}-${minutes_whole}-${seconds}' // Following the JavaScript for string interpolation
+                    bearing = 'N ${dms} E'
                 } else if ( azimuth > 270 && azimuth < 360 ) {
                     azimuth = 360 - azimuth
                     azimuth = float(azimuth)
                     degree = int(azimuth)
                     minutes = (azimuth - degree) * 60
                     minutes_whole = int(minutes)
-                    seconds = round(float((minutes - minutes_whole) * 60),2)
-                    dms = f"{degree}-{minutes_whole}-{seconds}"
-                    bearing = 'S {{ ^5} E'.format(dms)
+                    seconds = (float((minutes - minutes_whole) * 60).toPrecision(2))
+                    dms = '${degree}-${minutes_whole}-${seconds}' // Following the JavaScript for string interpolation
+                    bearing = 'S ${dms} E'
                 } else if ( azimuth == 0 ) {
                     bearing = "DUE SOUTH"
                 } else if ( azimuth == 90 ) {
@@ -107,68 +102,71 @@ function AzimuthToBearing(azimuth) {
                 } else ( azimuth == 360 ) 
                     bearing = "DUE SOUTH"
                 return bearing, azimuth, azimuth_uncon
+                
+                return [bearing, azimuth, azimuth_uncon]}
     
-    else if { // In DD form 
+    else { // In DD form 
                 // Convert DD to DMS
                 azimuth = float(azimuth) 
                 azimuth = (azimuth) % 360
-                azimuth_uncon = azimuth // make a variable of the unconverted azimuth for lat && dep
+                let azimuth_uncon = azimuth // make a variable of the unconverted azimuth for lat and dep
                 /*
-                Identif (y the bearing && orientation of the DD angle
-                // 1st - convert azimuth to bearing in DD, &&  identif (y direction
+                Identify the bearing && orientation of the DD angle
+                // 1st - convert azimuth to bearing in DD, and  identify direction
                 // 2nd - convert the DD angle from 1st to DMS
                 */
-                if ( azimuth > 0 && azimuth < 90{
-                    degree = int(azimuth)
-                    minutes = (azimuth - degree) * 60
-                    minutes_whole = int(minutes)
-                    seconds = round(float((minutes - minutes_whole) * 60),2)
-                    dms = f"{degree}-{minutes_whole}-{seconds}"
-                    bearing = 'S {{ ^5} W'.format(dms)
-                else if ( azimuth > 90 && azimuth < 180{
+                if ( azimuth > 0 && azimuth < 90 ) {
+                    let degree = int(azimuth)
+                    let minutes = (azimuth - degree) * 60
+                    let minutes_whole = int(minutes)
+                    let seconds = (float((minutes - minutes_whole) * 60).toPrecision(2))
+                    let dms = '${degree}-${minutes_whole}-${seconds}'
+                    bearing = 'S ${dms} W'
+                } else if ( azimuth > 90 && azimuth < 180 ) {
                     azimuth = 180 - azimuth
                     azimuth = float(azimuth)
-                    degree = int(azimuth)
-                    minutes = (azimuth - degree) * 60
-                    minutes_whole = int(minutes)
-                    seconds = round(float((minutes - minutes_whole) * 60),2)
-                    dms = f"{degree}-{minutes_whole}-{seconds}"
-                    bearing = 'N {{ ^5} W'.format(dms)
-                else if ( azimuth > 180 && azimuth < 270{
+                    let degree = int(azimuth)
+                    let minutes = (azimuth - degree) * 60
+                    let minutes_whole = int(minutes)
+                    let seconds = (float((minutes - minutes_whole) * 60).toPrecision(2))
+                    dms = '${degree}-${minutes_whole}-${seconds}'
+                    bearing = 'N ${dms} W'
+                } else if ( azimuth > 180 && azimuth < 270 ) {
                     azimuth = azimuth - 180
                     azimuth = float(azimuth)
                     degree = int(azimuth)
                     minutes = (azimuth - degree) * 60
                     minutes_whole = int(minutes)
                     seconds = round(float((minutes - minutes_whole) * 60),2)
-                    dms = f"{degree}-{minutes_whole}-{seconds}"
-                    bearing = 'N {{ ^5} E'.format(dms)
-                else if ( azimuth > 270 && azimuth < 360{
+                    dms = '${degree}-${minutes_whole}-${seconds}'
+                    bearing = 'N ${dms} E'
+                } else if ( azimuth > 270 && azimuth < 360 ) {
                     azimuth = 360 - azimuth
                     azimuth = float(azimuth)
                     degree = int(azimuth)
                     minutes = (azimuth - degree) * 60
                     minutes_whole = int(minutes)
                     seconds = round(float((minutes - minutes_whole) * 60),2)
-                    dms = f"{degree}-{minutes_whole}-{seconds}"
-                    bearing = 'S {{ ^5} E'.format(dms)
-                else if ( azimuth == 0{
+                    dms = '${degree}-${minutes_whole}-${seconds}'
+                    bearing = 'S ${dms} E'
+                } else if ( azimuth == 0 ) {
                     bearing = "DUE SOUTH"
-                else if ( azimuth == 90{
+                } else if ( azimuth == 90 ) {
                     bearing = "DUE WEST"
-                else if ( azimuth == 180{
+                } else if ( azimuth == 180 ) {
                     bearing = "DUE NORTH"
-                else if ( azimuth == 270{
+                } else if ( azimuth == 270 ) {
                     bearing = "DUE EAST"
-                else if ( azimuth == 360{
+                } else if ( azimuth == 360 ){
                     bearing = "DUE SOUTH"
-    
-                return bearing, azimuth, azimuth_uncon
+                } 
+                return [bearing, azimuth, azimuth_uncon]
 }
+
 //  Create a sentinel controlled loop
 counter1 = 1
 counter2 = 2
-counter = f"{counter1}-{counter2}"
+counter = '${counter1}-${counter2}'
 var lines = []
 var distance_list =[] //list of distance to be used for compass traverse adjustment
 var sumLat = 0
@@ -187,88 +185,70 @@ var data  = [
 [10.99, 180.000]
 [41.40, 50.562]
 ]
-while true {
-        console.log()
-        console.log("LINE NO. ", counter)    
-        // Ask for distance input
-        try{
-            distance_input = float(input("Enter Distance{ "))
-        // if ( distance is invalid, ask for input again
-        except ValueError {
-            console.log("INVALID{ You need to enter a number.")
-            console.log()
-        else {
-            azimuth = input("Enter Azimuth from the South{ ")
-            distance_list.push(distance_input) // add the distance input of the user to the distance list
-        // Extract bearing, lat, && dep values
-            bearing, azimuth, azimuth_uncon = AzimuthToBearing(azimuth)
-            latitude = getLatitude(azimuth=float(azimuth_uncon), distance=float(distance_input)) 
-            departure = getDeparture(azimuth=float(azimuth_uncon), distance=float(distance_input))
 
-        // Get summation of latitude && departure
-        sumLat += latitude
-            // sumLat = sumLat + Lat
-        sumDep += departure
-            // sumDep + sumDep + Dep
-        sumDist += float(distance_input)
-        
-        let line = [counter, distance_input, bearing, latitude, departure]  // Create a list of the line
-        lines.push(line)
+for (var i = 0; i < data.length; i++) {
+    let line = data [i]
+    let distance = line[0]
+    let azimuth = line[1]
+    let bearing = AzimuthToBearing(azimuth)
+    let latitude = getLatitude(distance, azimuth)
+    let departure = getDeparture(distance, azimuth)
 
-        // Ask for input
-        YN = input("Add new line? (Y/N) ")
-        if ( YN.lower() == "yes" or YN.lower() == "y"{
-                counter1 += 1
-                counter2 += 1
-                counter = f"{counter1}-{counter2}"
-                continue
-        else{
-                break
-
-    // Adjust the traverse 
-for line in lines {
-    distance = distance_list[row]
-    // Solve for correction of latitude per row
-    cLat = - sumLat * (distance/sumDist)
-    cDep = - sumDep * (distance/sumDist)
-
-    // solve for adjusted latitude && departure
-    latitude = line[3]
-    departure = line[4]
-    adjLat = latitude + cLat
-    adjDep = departure + cDep
-        
     // Get summation of latitude && departure
-    sumAdjLat += adjLat
-    sumAdjDep += adjDep
-    
-    adjLat = round(adjLat,7)
-    adjDep = round(adjDep,7)
-    // Add the adjusted latitude && departure to the table (push)
-    line.push(adjLat)
-    line.push(adjDep)
-    row += 1
-    tuple(line) // convert the list of line to a tuple
+    sumLat += latitude
+    // sumLat = sumLat + Lat
+    sumDep += departure
+    // sumDep + sumDep + Dep
+    sumDist += float(distance)
+
+    lines.push([(i+1), distance, bearing, latitude, departure])
+}
+    // Adjust the traverse 
+    for (let i = 0; i < lines.length; i++) {
+        let line = lines[i]
+        let distance = distance_list[row]
+        // Solve for correction of latitude per row
+        let cLat = - sumLat * (distance/sumDist)
+        let cDep = - sumDep * (distance/sumDist)
+
+        // solve for adjusted latitude && departure
+        let latitude = line[3]
+        let departure = line[4]
+        let adjLat = latitude + cLat
+        let adjDep = departure + cDep
+            
+        // Get summation of latitude && departure
+        sumAdjLat += adjLat
+        sumAdjDep += adjDep
+        
+        adjLat = (adjLat.toPrecision(5))
+        adjDep = (adjDep.toPrecision(5))
+        // Add the adjusted latitude && departure to the table (push)
+        line.push(adjLat)
+        line.push(adjDep)
+        row += 1
+}
+
 console.log()
 console.log("-----------------------------------------------------------------------------------------------------------------------------------------------")
-console.log('{{ ^15} {{ ^15} {{ ^15} {{ ^20} {{ ^23} {{ ^23} {{ ^23} '.format("LINE NO. ", "DISTANCE ", "BEARING ", "LATITUDE ", "DEPARTURE ", "ADJUSTED LATITUDE ", "ADJUSTED DEPARTURE "))
+console.log("LINE NO. ".padEnd(15), "DISTANCE ".padEnd(15), "BEARING ".padEnd(15), "LATITUDE ".padEnd(15), "DEPARTURE ".padEnd(15), "ADJUSTED LATITUDE ".padEnd(15), "ADJUSTED DEPARTURE ".padEnd(15))
 console.log("-----------------------------------------------------------------------------------------------------------------------------------------------")
 
-for line in lines{
-    console.log('{{ ^13} {{ ^16} {{ ^16} {{ ^20} {{ ^23} {{ ^23} {{ ^23} '.format(line[0], line[1], line[2], line[3], line[4], line[5], line[6]))
+for (var line of lines) {
+    console.log(line[0].toString().padEnd(13), line[1].toString().padEnd(16), line[2].toString().padEnd(16), line[3].toString().padEnd(20), line[4].toString().padEnd(23), line[5].toString().padEnd(23), line[6].toString().padEnd(23))
+}
 
 console.log("-----------------------------------------------------------------------------------------------------------------------------------------------")
-console.log("Summation of Latitude{ ", round(sumLat,7))
-console.log("Summation of Departure{ ", round(sumDep,7))
-console.log("Summation of Distance{ ", round(sumDist,3))
+console.log("Summation of Latitude{ ", sumLat.toPrecision(5))
+console.log("Summation of Departure{ ", sumDep.toPrecision(5))
+console.log("Summation of Distance{ ", sumDist.toPrecision(5))
 
-console.log("Summation of Adjusted Latitude{ ", round(sumAdjLat,7))
-console.log("Summation of Adjusted Departure{ ", round(sumAdjDep,7))
+console.log("Summation of Adjusted Latitude{ ", sumAdjLat.toPrecision(5))
+console.log("Summation of Adjusted Departure{ ", sumAdjDep.toPrecision(5))
 console.log()
-LEC = sqrt(sumLat**2 + sumDep**2)
-console.log("LEC{ ", LEC)
-REC = sumDist/LEC
-console.log("REC{ 1{ ", round(REC, -3)) 
+let LEC = Math.sqrt(sumLat**2 + sumDep**2)
+console.log("LEC: ", LEC)
+let REC = sumDist/LEC
+console.log("1: ", (Math.floor(REC/1000)*1000).toPrecision(-3))
 
-console.log()
-console.log("---------------------END-----------------------")
+}
