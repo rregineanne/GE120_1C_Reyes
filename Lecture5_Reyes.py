f9214bbec599de4dfb1280c68da30d01e8ce8a76
rregineanne <rcreyes18@up.edu.ph>
2024-05-21 12:38:54 +0800

    Added lecture 5 notes

--- Lecture5_Reyes.py
+++ Lecture5_Reyes.py
@@ -50,3 +50,95 @@ print("Hero Specialty:", hero.specialty.split("/"))
 print("Hero Offense+difficulty:", hero.statistics["offense"] + hero.statistics["difficulty"])
 
 # hero.superSkill("Zilong")
+# OBJECT ORIENTED PROGRAMMING
+
+#creating classes
+class Crayon:
+    pass
+
+redCrayon = Crayon()
+
+# print(type(redCrayon))
+
+# creating ML HEROES
+
+class MLHero:
+    def __init__(self, name, description="Twilight Goddess", offense=80):
+        self.name = name
+        self.description = description
+        role = "Mage"
+        self.role = role.upper()[0]
+        self.specialty = "Damage/Poke"
+        self.statistics = {
+            "durability": 60,
+            "offense": offense,
+            "skill_effects": 50,
+            "difficulty": 70
+        }
+
+    def __str__(self):
+        return(self.name + ", the " + self.description)
+
+    def __add__(self, other):
+        return(self.name + " and " + other.name + " combined!")
+
+    def __gt__(self, other):
+        if (self.statistics["offense"] > other.statistics["offense"]):
+            return(self.name + " ATTACKED AND WON!")
+        else:
+            return(self.name + "lost. " + other.name + " is too strong!")
+
+    def skill(self):
+        print(self.name + " used the ATTACK!!")
+
+    def superSkill(self, opponent):
+        print(self.name + " used SUPERSKILL against " + opponent)
+
+    def split(self):
+        print("MASAKIT Mag split")
+
+class Tank(MLHero):
+    def __init__(self, name, description="Twilight Goddess", offense=80):
+        super().__init__(name, description, offense)
+        self.role = "Tank"
+
+# lunox = MLHero("Lunox")
+# aldous = MLHero("Aldous", "Demon Slayer", 90)
+
+tigreal = Tank("Tigreal")
+tigreal.skill()
+
+# print(aldous + lunox)
+# print(lunox > aldous)
+
+# print(aldous.__name)
+# aldous.__name = "BOGART"
+# print(aldous.__name)
+
+# print(dir(aldous))
+
+# class Spam:
+#     def __init__(self):
+#         self.__egg=7
+
+#     def print_egg(self):
+#         print(self.__egg)
+
+# s = Spam()
+# s.print_egg()
+# print(s._Spam__egg)
+
+
+# print("Hero Name:", hero.name)
+# print("Hero Desc:", hero.description)
+# print("Hero Role:", hero.role)
+# print("Hero Specialty:", hero.specialty.split("/"))
+# print("Hero Offense+Difficulty:", hero.statistics["offense"] + hero.statistics["difficulty"])
+
+# hero.superSkill("Zilong")
+# hero.split()
+
+# split is just a method of the class String
+# text = "HELLO,PLUS,ME"
+# text.split()
+
