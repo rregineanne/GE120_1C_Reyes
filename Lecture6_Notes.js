01b58af838fc423256f41af082923f146ab81759
rregineanne <rcreyes18@up.edu.ph>
2024-05-21 12:39:39 +0800

    Added lecture 6 notes

--- Lecture6_Notes.js
+++ Lecture6_Notes.js
@@ -50,3 +50,60 @@ function getLatitude(distance, azimuth) {
     return latitude
 }
 // console.log(getLatitude(12, 50))
+
+
+/*
+LECTURE 6: CODING WITH JAVASCRIPT
+*/
+
+// console.log("HELLO MADLANGPEOPLE MABUHAY")
+
+// creating variables
+let x = 5
+var y = 5
+const z = 7
+
+// console.log(x, y, z)
+
+// if statements
+if (x == y) {
+    console.log(x.toString().concat(" is equal to ", y.toString()))
+} else if (x == z) {
+    console.log(x.toString().concat(" is equal to ", z.toString()))
+} else {
+    console.log(x.toString().concat(" is not equal to anything"))
+}
+
+const students = ["peter", "maja", "aj", "quinmar"]
+console.log(students.length)
+
+
+// COMPARING TWO VALUES AND DATA TYPES
+console.log(0 == "0")
+console.log(0 === "0")
+
+for (var i = 1; i > z; i++){
+    console.log(i)
+}
+
+for (var student of students){
+    for (var i = 0; i < student.length; i++){
+        console.log(student[i])
+    }
+}
+
+console.log(students[2])
+
+function degreesToRadians(num) {
+    var value = num * Math.PI / 180
+    return value
+}
+
+console.log(degreesToRadians(180))
+
+function getLatitude(distance, azimuth) {
+    var latitude = - distance * Math.cos(degreesToRadians(azimuth))
+    return latitude
+}
+
+console.log(getLatitude(12, 50))
